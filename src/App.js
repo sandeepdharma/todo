ad574626943bd1e01dd0976f5a49446465bd7971
sandeepdharma <spandala@msystechnologies.com>
2024-12-27 20:57:31 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [iconBlur, setIconBlur] = useState(false);
 
   const getFormData = (data) => {
+    console.log(data);
     setTodoData(data);
     localStorage.setItem("todoData", JSON.stringify(data));
     setFilterData(data);
